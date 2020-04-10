30fdd895f2f2b51cfa163be75717e83d574810c9
sanjujaiswal <sanjayjaiswal130082@gmail.com>
2020-04-10 14:03:17 +0530

    ADDED: Resetting board and start game

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 echo "-----Welcome to Tic-Tac-Toe Game-----"
+
+#Function to reset board
+function resetBoard(){
+	echo "Tic-Tac-Toe Game"
+	player=x;
+	gameStatus=1;
+	declare -A board;
+}
+
+resetBoard
