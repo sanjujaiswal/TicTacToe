0cf601a9cf9f99d0b0dd6b91428a7c37326ec333
sanjujaiswal <sanjayjaiswal130082@gmail.com>
2020-04-13 01:04:45 +0530

    ADDED: Toss to who play first

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -1,10 +1,25 @@
-#!/bin/bash
+#!/bin/bash -x
 echo "-----Welcome to Tic-Tac-Toe Game-----"
+
+#Constants declarations
+NUMBER=2
+#Variables declarations
+tossPlayer=0
+
+#Toss to play first
+function tossToPlayFirst(){
+	tossPlayer=$((RANDOM%NUMBER))
+	if [ $tossPlayer -eq 0 ]
+	then
+		echo "x"
+	else
+		echo "0"
+	fi
+}
 
 #Function to reset board
 function resetBoard(){
-	echo "Tic-Tac-Toe Game"
-	currentPlayer=x;
+	currentPlayer=$( tossToPlayFirst );
 	gameStatus=1;
 	declare -a board;
 }
