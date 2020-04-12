60f57a4fe4d9f5778d853eca16d7b1b3c0819155
sanjujaiswal <sanjayjaiswal130082@gmail.com>
2020-04-13 03:15:19 +0530

    ADDED: Check move of player for win,tie or player turn

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -1,12 +1,14 @@
-#!/bin/bash
+#!/bin/bash -x
 echo "-----Welcome to Tic-Tac-Toe Game-----"
 
 #Constants declarations
 NUMBER=2
-ROW=3
-COL=3
+TOTAL_COUNT=9
 #Variables declarations
 tossPlayer=0
+moveCount=1
+#Array declaration
+declare -a board
 
 #Toss to play first
 function tossToPlayFirst(){
@@ -40,7 +42,88 @@ function initialize(){
       echo "| ${board[@]} |"
    done
 	echo "---------"
+		displayBoard
 }
 
+#Function to print board
+function displayBoard(){
+echo "---------------"
+	for (( row=1;row<=3;row++ ))
+	do
+		for (( column=1;column<=3;column++ ))
+		do
+			echo -e "| ${board[$row,$column]} | \c"
+		done
+		echo
+	done
+	echo "---------------"
+}
+
+#Change the player turn
+function changePlayer(){
+	if [[ $1 == "x" ]]
+	then
+		currentPlayer="0";
+	else
+		currentPlayer="x";
+	fi
+}
+
+#Function to check win condition
+function checkWin(){
+	gameStatus=0;
+	for (( i=1;i<=3;i++ ))
+	do
+		if [[ ${board[$i,1]} == $currentPlayer && ${board[$i,1]} == ${board[$i,2]} && ${board[$i,1]} == ${board[$i,3]} ]]
+		then
+			gameStatus=1;
+		fi
+		if [[ ${board[1,$i]} == $currentPlayer && ${board[1,$i]} == ${board[2,$i]} && ${board[1,$i]} == ${board[3,$i]} ]]
+		then
+			gameStatus=1;
+		fi
+	done
+
+	if [[ ${board[1,1]} == $currentPlayer &&  ${board[1,1]} == ${board[2,2]} && ${board[1,1]} == ${board[3,3]} ]]
+	then
+		gameStatus=1;
+	fi
+
+	if [[ ${board[1,3]} == $currentPlayer && ${board[1,3]} == ${board[2,2]} && ${board[1,3]} == ${board[3,1]} ]]
+	then
+		gameStatus=1;
+	fi
+
+	echo $gameStatus
+}
+
+#Function to place mark on board
+function placeMark(){
+	if [[ ${board[$1,$2]} == - ]]
+	then
+		board[$1,$2]=$currentPlayer
+		displayBoard
+		if [[ $( checkWin $(()) ) -eq 1 ]]
+		then
+			echo "$currentPlayer wins!!"
+			exit
+		fi
+		changePlayer $currentPlayer
+	else
+		echo "Position already occupied"
+	fi
+}
+
+#start execution
 resetBoard
-echo $currentPlayer
+while [[ $moveCount -le $TOTAL_COUNT ]]
+do
+	read -p "Enter row no. " row
+	read -p "Enter column no. " column
+	placeMark $row $column
+	((moveCount++))
+done
+if [[ $gameStatus -eq 0 ]]
+then
+	echo "Match tie !!! "
+fi
