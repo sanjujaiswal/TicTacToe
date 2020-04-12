e7734747efb5eaa38fa122ef189914bcf617f9ab
sanjujaiswal <sanjayjaiswal130082@gmail.com>
2020-04-13 01:36:23 +0530

    ADDED: Display the board

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -1,8 +1,10 @@
-#!/bin/bash -x
+#!/bin/bash
 echo "-----Welcome to Tic-Tac-Toe Game-----"
 
 #Constants declarations
 NUMBER=2
+ROW=3
+COL=3
 #Variables declarations
 tossPlayer=0
 
@@ -22,6 +24,22 @@ function resetBoard(){
 	currentPlayer=$( tossToPlayFirst );
 	gameStatus=1;
 	declare -a board;
+	initialize
+}
+
+#Initialize to board
+function initialize(){
+	echo "Enter position on Board :"
+   for (( rowPosition=1;rowPosition<=3;rowPosition++ ))
+   do
+      echo "---------"
+      for (( columnPosition=1;columnPosition<=3;columnPosition++ ))
+      do
+         board[$rowPosition,$columnPosition]="-"
+      done
+      echo "| ${board[@]} |"
+   done
+	echo "---------"
 }
 
 resetBoard
